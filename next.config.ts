de71b9005c92e6ae93aa5c7d8a9b3fe12f329d35
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-06-22 20:17:56 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: "/SchemaForge",
+    basePath: process.env.NODE_ENV === "production" ? "/SchemaForge" : "",
 };
 
 export default nextConfig;
